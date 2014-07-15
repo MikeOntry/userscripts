a5f6d5bc9916bfe51da4a965b171699d5ca7d2a9
stil <przemekryciuk@gmail.com>
2014-07-15 22:38:30 +0200

    Lightened source code
    Uses existing jQuery from website and handles iframe errors on homepage.

--- gwardia.user.js
+++ gwardia.user.js
@@ -2,34 +2,28 @@
 // @name          Gwardia
 // @description   Twitter Gwardii
 // @include       http://www.erepublik.com/*
-// @version       1.2.0
+// @require       https://github.com/erpk/userscripts/raw/master/injection.user.js
+// @version       1.2.1
 // ==/UserScript==
 
-function addJQuery(callback) {
-  var script = document.createElement('script');
-  script.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js');
-  script.addEventListener('load', function () {
-    var script = document.createElement('script');
-    script.textContent = 'window.jQ=jQuery.noConflict(true);(' + callback.toString() + ')();';
-    document.body.appendChild(script);
-  }, false);
-  document.body.appendChild(script);
-}
-
-addJQuery(function () {
-  jQ(document).ready(function () {
-    var column = jQ('#content div.column').eq(1);
+function main() {
+  if (!jQuery) return;
+  jQuery(document).ready(function () {
     var baseUrl = 'http://gwardia.org';
+    var column = jQuery('#content div.column').eq(1);
+    if (column.length == 0) return;
     column.prepend(
       '<div style="width:409px;height:200px;">' +
       '<iframe scrolling="no" style="border:0;width:100%;height:100%;" src="' + baseUrl + '/feed"></iframe>' +
       '</div>'
     );
     var img = new Image();
-    img.src = baseUrl + '/log?' + jQ.param({
-      citizenId: ErpkPvp.citizenId,
-      remainingFood: food_remaining,
-      currentEnergy: globalNS.userInfo.wellness
+    img.src = baseUrl + '/log?' + jQuery.param({
+      citizenId: erepublik.citizen.citizenId,
+      currentEnergy: erepublik.citizen.energy,
+      remainingFood: erepublik.citizen.energyToRecover,
     });
   });
-});
+}
+
+contentEval(main);
