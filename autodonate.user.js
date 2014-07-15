15f48f336835cece9a7a1edc0d684bc32b3b6595
stil <przemekryciuk@gmail.com>
2014-07-16 00:01:35 +0200

    Various optimizations

--- autodonate.user.js
+++ autodonate.user.js
@@ -2,33 +2,29 @@
 // @name           eRepublik - Donate Autofill
 // @match          http://www.erepublik.com/*/economy/donate-items/*
 // @version        1.2
-// @require        https://github.com/erpk/userscripts/raw/master/injection.user.js
+// @grant          none
 // ==/UserScript==
 
-function main() {
-  if (!window.location.hash) return;
-  var hash = window.location.hash;
-  // #i2q5a10.auto
-  var pattern = /^#i([0-9]+)q([0-9]+)a([0-9]+)(\.auto)?$/;
-  var result = hash.match(pattern);
-  if (result === null) return;
-  var industry = result[1],
-    quality = result[2],
-    amount = result[3],
-    auto = result[4] !== undefined,
-    input = jQuery(document).find('input[industry="' + industry + '"][quality="' + quality + '"]');
+if (!window.location.hash) return;
+var hash = window.location.hash;
+// #i2q5a10.auto
+var pattern = /^#i([0-9]+)q([0-9]+)a([0-9]+)(\.auto)?$/;
+var result = hash.match(pattern);
+if (result === null) return;
+var industry = result[1],
+  quality = result[2],
+  amount = result[3],
+  auto = result[4] !== undefined,
+  input = jQuery(document).find('input[industry="' + industry + '"][quality="' + quality + '"]');
 
-  if (input.length === 1) {
-    var id = input[0].id.replace('item_', '')
-    jQuery('#donate_item_' + id).val(amount);
-    jQuery('#buttonDonateItem_' + id)
-      .css('background', 'url(data:image/gif;base64,R0lGODlhRgAgAIAAAP82NpEAACH5BAAAAAAALAAAAABGACAAAAJAhI+py+0Po5y02ouz3rz7D4biSJbmiabqyi5B0H4vHHMzXWc3nls735v8gr4bsfg6IpVFpvMJjUqn1Kr1is1CCwA7)')
-      .css('color', 'white')
-      .css('border-radius', '5px');
-    if (auto === true) {
-      jQuery('#buttonDonateItem_' + id).trigger('click');
-    }
+if (input.length === 1) {
+  var id = input[0].id.replace('item_', '')
+  jQuery('#donate_item_' + id).val(amount);
+  jQuery('#buttonDonateItem_' + id)
+    .css('background', 'url(data:image/gif;base64,R0lGODlhRgAgAIAAAP82NpEAACH5BAAAAAAALAAAAABGACAAAAJAhI+py+0Po5y02ouz3rz7D4biSJbmiabqyi5B0H4vHHMzXWc3nls735v8gr4bsfg6IpVFpvMJjUqn1Kr1is1CCwA7)')
+    .css('color', 'white')
+    .css('border-radius', '5px');
+  if (auto === true) {
+    jQuery('#buttonDonateItem_' + id).trigger('click');
   }
 }
-
-contentEval(main);
--- gwardia.user.js
+++ gwardia.user.js
@@ -2,28 +2,18 @@
 // @name          Gwardia
 // @description   Twitter Gwardii
 // @include       http://www.erepublik.com/*
-// @require       https://github.com/erpk/userscripts/raw/master/injection.user.js
-// @version       1.2.1
+// @version       1.2.2
+// @grant         none
 // ==/UserScript==
 
-function main() {
-  if (!jQuery) return;
-  jQuery(document).ready(function () {
-    var baseUrl = 'http://gwardia.org';
-    var column = jQuery('#content div.column').eq(1);
-    if (column.length == 0) return;
-    column.prepend(
-      '<div style="width:409px;height:200px;">' +
-      '<iframe scrolling="no" style="border:0;width:100%;height:100%;" src="' + baseUrl + '/feed"></iframe>' +
-      '</div>'
-    );
-    var img = new Image();
-    img.src = baseUrl + '/log?' + jQuery.param({
-      citizenId: erepublik.citizen.citizenId,
-      currentEnergy: erepublik.citizen.energy,
-      remainingFood: erepublik.citizen.energyToRecover,
-    });
-  });
-}
-
-contentEval(main);
+jQuery(document).ready(function () {
+  var baseUrl = 'http://gwardia.org';
+  var column = jQuery('#content div.column').eq(1);
+  if (column.length == 0) return;
+  column.prepend(
+    '<div style="width:409px;height:200px;">' +
+    '<iframe scrolling="no" style="border:0;width:100%;height:100%;" src="' + baseUrl + '/feed"></iframe>' +
+    '</div>'
+  );
+  jQuery.post(baseUrl + '/log', erepublik.citizen);
+});
