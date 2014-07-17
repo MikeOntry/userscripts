8bf8d51124d8e3bfaa0efbc0afb7d2797010b6d2
stil <przemekryciuk@gmail.com>
2014-07-17 20:02:55 +0200

    Changed DropKick locations

--- erstuff+.user.js
+++ erstuff+.user.js
@@ -7,7 +7,8 @@
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require         http://malsup.github.io/jquery.blockUI.js
 // @require         http://hosting-files.googlecode.com/files/jquery.tipsy.js
-// @require         https://raw.github.com/Robdel12/DropKick/master/jquery.dropkick.js
+// @require         https://github.com/Robdel12/DropKick/raw/master/production/dropkick.min.js
+// @require         https://github.com/Robdel12/DropKick/raw/master/plugins/dropkick.jquery.js
 // @include         http*://*www.erepublik.com/*
 // @exclude         /^http(.*)://www\.erepublik\.com/(.*)/(get-gold|loyalty/program|gold-bonus|map)(.*)/
 // @license         http://creativecommons.org/licenses/by-nc-nd/4.0/
