d4a7bee685a736c501abe3d7f298e42cf60171a9
stil <przemekryciuk@gmail.com>
2014-07-26 21:58:43 +0200

    Improved autopm, removed injection

--- autopm.user.js
+++ autopm.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           eRepublik - PM Autofill
 // @include        http://www.erepublik.com/*/main/messages-inbox*
-// @version        1.2.7
+// @version        1.3.0
 // @grant          none
 // ==/UserScript==
 
@@ -9,45 +9,28 @@ function b64_to_utf8(str) {
   return unescape(decodeURIComponent(window.atob(str)));
 }
 
-var closeWindow = false;
-var ajaxLoad = function (data) {
-  data = data.replace('var href = window.location.href;', 'var href = "";');
-  jQuery('.message_ajax_container').slideUp('fast', function () {
-    jQuery('.message_ajax_container').html(data);
-    jQuery('.message_ajax_container').slideDown('fast', function () {
-      if (jQuery('.message_unread:first').length != 0) {
-        jQuery(window).scrollTop(jQuery('.message_unread:first').offset().top);
-      }
-    });
-
-    jQuery('#content form').submit(function (ev) {
-      ev.preventDefault();
-      handle_form(ev.target.children[0]);
-    });
-    ajaxify_messages();
-    submit_form();
-
-    jQuery('#citizen_subject').val(query.subject);
-    jQuery('#citizen_message').val(query.content);
-
-    if (closeWindow === true) {
-      window.setTimeout(
-        function () {
-          window.close();
-        },
-        1000
-      );
-    } else if (query.auto === true) {
-      jQuery(document).ready(function () {
-        closeWindow = true;
-        jQuery('.message_submit').trigger('click');
-      });
-    }
-  });
-}
-
 if (window.location.hash) {
-  var json = b64_to_utf8(window.location.hash.substr(1));
-  var query = JSON.parse(json);
-  jQuery.get('/' + erepublik.settings.culture + '/main/messages-compose/' + query.id, ajaxLoad);
+  var pm = JSON.parse(b64_to_utf8(window.location.hash.substr(1))),
+    $ = jQuery;
+  pm.filled = false;
+  history.pushState("", document.title, window.location.pathname + window.location.search); // remove hash from URL
+
+  $(document).ready(function() {
+    var a = $('a[href$="messages-compose/0"]');
+    a.attr('href', a.attr('href').replace('0', pm.id));
+    var intervalId = window.setInterval(function() {
+      if ($('.message_unread').length > 0) {
+        window.clearInterval(intervalId);
+        window.close();
+      } else if (!pm.filled && $('#message_form').length > 0) {
+        $('#citizen_subject').val(pm.subject);
+        $('#citizen_message').val(pm.content);
+        pm.filled = true;
+        if (pm.auto === true) {
+          $('#message_form .message_submit').trigger('click');
+        }
+      }
+    }, 50);
+    a.trigger('click');
+  });
 }
